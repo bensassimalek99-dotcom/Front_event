d95b048a9245c4fa078ccc1ef2a37cd44b71df38
pc <bensassimalek99@gmail.com>
2026-05-01 21:55:19 -0500

    Update backend URL to v2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://eventura-backend-hfa0g8f7cvcfhmce.italynorth-01.azurewebsites.net/api'
+  apiUrl: 'https://eventura-backend-v2.azurewebsites.net/api'
 };
